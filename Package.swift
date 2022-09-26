07bd915cfae42d949d1d348368526fae6a84cf9d
YOUNG MO SONG <77970826+mooyoung2309@users.noreply.github.com>
2022-09-26 18:39:39 +0900

    create pakage.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+import PackageDescription
+
+let package = Package(
+    name: "NMapsMap",
+    defaultLocalization: "ko",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "NMapsMap",
+            targets: [
+                "NMapsMap",
+            ]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "NMapsMap",
+            url: "https://github.com/UMC-1-Footprint/NMapsMap/releases/download/3.16.0/NMapsMap.xcframework.zip",
+            checksum: "efa36d05b32ec32eef6e26047d3830ace3906cd07e62a848ca9db59f02f0a2e4"
+        ),
+    ]
+)
