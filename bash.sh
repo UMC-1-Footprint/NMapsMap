75ccb8e0a57d785435c7aaf95544b5bbbcff5179
YOUNG MO SONG <77970826+mooyoung2309@users.noreply.github.com>
2022-09-26 19:18:19 +0900

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+VERSION="release/3.16.0"
+
+git clone --depth=1 --branch="${VERSION}" https://github.com/navermaps/NMapsMap.git
+
+mv NMapsMap/framework/NMapsMap.xcframework NMapsMap.xcframework
+
+zip -r NMapsMap.xcframework.zip NMapsMap.xcframework
+
+rm -rf NMapsMap.xcframework
+
+rm -rf NMapsMap
+
+swift package compute-checksum NMapsMap.xcframework.zip
